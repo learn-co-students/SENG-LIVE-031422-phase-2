0b27c568e8507c90e300701cbffd4ddadc77fd57
Aysan Isayo <aisayo@icloud.com>
2022-04-08 16:08:40 -0400

    lecture solution

--- 05-react-forms/project_showcase/src/App.js
+++ 05-react-forms/project_showcase/src/App.js
@@ -7,6 +7,7 @@ import ProjectList from "./components/ProjectList";
 const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(true);
   const [projects, setProjects] = useState([]);
+
   const fetchProjects = () => {
     fetch("http://localhost:4000/projects")
       .then((resp) => resp.json())
@@ -17,10 +18,17 @@ const App = () => {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   };
 
+  // create a function that will update the state of projects so that App still has control over this update and then send that function down to projectform
+
+  const onAddProject = (newProj) => {
+    // if we are adding a new project to projects collection, what should we pass to onAddProject
+    setProjects(projects => [...projects, newProj]) 
+  }
+
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm />
+      <ProjectForm onAddProject={onAddProject}/>
       <button onClick={fetchProjects}>Load Projects</button>
       <ProjectList projects={projects} />
     </div>
--- 05-react-forms/project_showcase/src/components/ProjectForm.js
+++ 05-react-forms/project_showcase/src/components/ProjectForm.js
@@ -1,17 +1,90 @@
-const ProjectForm = () => {
+// we need to make this a controlled form:
+
+// First step: add state: import useState
+// Second step: is adding a name attribute to form inputs to reflect the key inside of our state object
+// Third step: attach an onChange event to each form field, and define an event handler to handle that onChange
+// Fourth step: inside of event handler for the onChange, update state
+
+// Now what do we do with the form data? Submit it!!!
+// First step: attach an event lister, onSubmit to the form
+// Second: create and event handler, prevent the default behavior
+
+import { useState } from "react";
+
+const ProjectForm = ({onAddProject}) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    about: "",
+    phase: "",
+    link: "",
+    image: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((formData) => ({ ...formData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData)
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(formData),
+    };
+
+    // I want to persist formData
+    fetch("http://localhost:4000/projects", configObj)
+    .then(resp => resp.json())
+    .then(project => {
+      // i need to use project to update the projects state in App component
+      // How???
+      // cb function 
+      onAddProject(project)
+      setFormData({
+        name: "",
+        about: "",
+        phase: "",
+        link: "",
+        image: "",
+      })
+    })
+  };
+
   return (
     <section>
-      <form className="form" autoComplete="off">
+      <form className="form" autoComplete="off" onSubmit={handleSubmit}>
         <h3>Add New Project</h3>
 
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" name="name" />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={handleChange}
+          value={formData.name}
+        />
 
         <label htmlFor="about">About</label>
-        <textarea id="about" name="about" />
+        <textarea
+          id="about"
+          name="about"
+          onChange={handleChange}
+          value={formData.about}
+        />
 
         <label htmlFor="phase">Phase</label>
-        <select name="phase" id="phase">
+        <select
+          name="phase"
+          id="phase"
+          onChange={handleChange}
+          value={formData.phase}
+        >
+          <option value="">Pick a Phase</option>
           <option value="1">Phase 1</option>
           <option value="2">Phase 2</option>
           <option value="3">Phase 3</option>
@@ -20,10 +93,22 @@ const ProjectForm = () => {
         </select>
 
         <label htmlFor="link">Project Homepage</label>
-        <input type="text" id="link" name="link" />
+        <input
+          type="text"
+          id="link"
+          name="link"
+          onChange={handleChange}
+          value={formData.link}
+        />
 
         <label htmlFor="image">Screenshot</label>
-        <input type="text" id="image" name="image" />
+        <input
+          type="text"
+          id="image"
+          name="image"
+          onChange={handleChange}
+          value={formData.image}
+        />
 
         <button type="submit">Add Project</button>
       </form>
